21c40bc8501fc30a3b1fbf70e32dd673bb44abe4
Johan Walles <johan.walles@gmail.com>
2025-09-21 14:47:44 +0200

    File switching placeholders

---
+++ internal/file-switching.go
@@ -0,0 +1,13 @@
+package internal
+
+func (p *Pager) previousFile() {
+	panic("unimplemented")
+}
+
+func (p *Pager) nextFile() {
+	panic("unimplemented")
+}
+
+func (p *Pager) firstFile() {
+	panic("unimplemented")
+}
