9668a69ab4db7e309c4e3514c4ce19e9d86ca8b3
Johan Walles <johan.walles@gmail.com>
2025-12-02 18:52:13 +0100

    Split matching benchmarks
    By case sensitiveness and substring vs regex.

--- internal/search/search_test.go
+++ internal/search/search_test.go
@@ -28,7 +28,7 @@ func TestToPattern(t *testing.T) {
 	assert.Assert(t, toPattern(")g").MatchString(")g"))
 }
 
-func BenchmarkMatch(b *testing.B) {
+func benchmarkMatch(b *testing.B, searchTerm string) {
 	sourceBytes, err := os.ReadFile("../../sample-files/large-git-log-patch-no-color.txt")
 	assert.NilError(b, err)
 
@@ -37,8 +37,7 @@ func BenchmarkMatch(b *testing.B) {
 
 	lines := strings.Split(fileContents, "\n")
 
-	// Same as in benchmarkSearch() in search-linescanner_test.go
-	search := For("This won't match anything")
+	search := For(searchTerm)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -46,4 +45,21 @@ func BenchmarkMatch(b *testing.B) {
 			search.Matches(line)
 		}
 	}
+}
+
+func BenchmarkCaseSensitiveSubstringMatch(b *testing.B) {
+	// Same as in benchmarkSearch() in search-linescanner_test.go
+	benchmarkMatch(b, "This won't match anything")
+}
+
+func BenchmarkCaseInsensitiveSubstringMatch(b *testing.B) {
+	benchmarkMatch(b, "this won't match anything")
+}
+
+func BenchmarkCaseSensitiveRegexMatch(b *testing.B) {
+	benchmarkMatch(b, "This [w]on't match anything")
+}
+
+func BenchmarkCaseInsensitiveRegexMatch(b *testing.B) {
+	benchmarkMatch(b, "this [w]on't match anything")
 }
