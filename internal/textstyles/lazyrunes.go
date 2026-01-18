3858707f809b2ab6ae3db1f784b7285465c5efc4
Johan Walles <johan.walles@gmail.com>
2026-01-18 09:01:56 +0100

    Partially implement lazyRunes

--- internal/textstyles/lazyrunes.go
+++ internal/textstyles/lazyrunes.go
@@ -1,18 +1,18 @@
 package textstyles
 
+import "unicode/utf8"
+
+// Length of the long consumeBullet() pattern
+const lookaheadBufferSize = 7
+
 // Lazily iterate a string by runes. Init by setting str only.
 type lazyRunes struct {
 	str string
 
-	rune0                  rune
-	rune0ByteIndex         int
-	rune0RuneIndexOneBased int
+	// From what position in the string should we pick the next rune?
+	nextByteIndex int
 
-	rune1          rune
-	rune1ByteIndex int
-
-	rune2          rune
-	rune2ByteIndex int
+	lookaheadBuffer []rune
 }
 
 func (l *lazyRunes) getRelative(runeIndex int) *rune {
@@ -20,9 +20,26 @@ func (l *lazyRunes) getRelative(runeIndex int) *rune {
 }
 
 func (l *lazyRunes) hasNext() bool {
-	FIXME
+	return l.nextByteIndex < len(l.str)
 }
 
 func (l *lazyRunes) next() {
-	FIXME
+	if l.lookaheadBuffer == nil {
+		// We're just getting started
+		l.lookaheadBuffer = make([]rune, 0, lookaheadBufferSize)
+	}
+
+	nextRune, runeSize := utf8.DecodeRuneInString(l.str[l.nextByteIndex:])
+	if runeSize == 0 {
+		// We are done
+		return
+	}
+
+	// Shift the lookahead buffer down
+	if len(l.lookaheadBuffer) == cap(l.lookaheadBuffer) {
+		l.lookaheadBuffer = l.lookaheadBuffer[1:]
+	}
+	l.lookaheadBuffer = append(l.lookaheadBuffer, nextRune)
+
+	l.nextByteIndex += runeSize
 }
