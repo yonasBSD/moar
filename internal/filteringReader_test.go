574ba31b1e9e8f2e8d44c5b87e08ca48a685e21f
Plutonium-239 <plutonium.239.811@gmail.com>
2026-03-01 17:27:27 +0530

    Benchmark FilteringReader.rebuildCache

--- internal/filteringReader_test.go
+++ internal/filteringReader_test.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/walles/moor/v2/internal/reader"
@@ -42,4 +44,31 @@ func TestZeroLines(t *testing.T) {
 	pager.filter = search.For("x")
 	rendered := pager.renderLines()
 	assert.Equal(t, len(rendered.lines), 0)
+}
+
+
+// Micro benchmark for rebuildCache (the main computation in filteringReader.go)
+func BenchmarkFilterHugeFile(b *testing.B) {
+	// The file packets_repeat.log is 18 kB and 90 lines long (with ANSI colors).
+	// To reach 300 MB, it will be repeated ~ 16000 times, resulting in a file with 1.44 M lines.
+	f, err := os.ReadFile("../sample-files/packets_repeat.log")
+	assert.NilError(b, err)
+	reqd_bytes := 300_000_000 // 300MB
+	text := strings.Repeat(string(f), reqd_bytes/len(f))
+	b.SetBytes(int64(len(text)))
+
+	input := reader.NewFromTextForTesting("BenchmarkFilterHugeFile()", text)
+	assert.NilError(b, input.Wait())
+
+	filterTerm := search.For("Periodic")
+	fr := FilteringReader{
+		BackingReader: input,
+		Filter:        &filterTerm,
+	}
+	fr.rebuildCache() // warmup
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fr.rebuildCache()
+	}
 }
